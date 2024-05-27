0366500036f4d14bfa8bbd605300b72629dc1bcb
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2024-05-27 19:50:19 +0530

    Initial Commit

---
+++ name_length.py
@@ -0,0 +1,27 @@
+# name = input("Enter your name : ")
+# print(name , " length is " , str(len(name)))
+
+# age = 21
+# if age >= 18:
+#     print("can vote")
+# elif age < 18:
+#     print("Can't vote")
+
+# marks = int(input("Enter marks : "))
+#
+# if marks >= 90:
+#     print('A')
+# elif (marks < 90) and (marks >= 80):
+#     print('B')
+# elif (marks >= 70) and (marks < 80):
+#     print('C')
+# else:
+#     print("fail")
+#
+
+num = int(input("Enter a number : "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
